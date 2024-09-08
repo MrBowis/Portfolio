0c84c68d9d4323d5ba01190520c0529769ddad37
MrBowis <02alejo20@gmail.com>
2024-09-08 17:16:30 -0500

    Add correct routes

--- src/pages/education.jsx
+++ src/pages/education.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/nav";
+import Footer from "../components/footer";
 
-export default function education() {
+export default function Education() {
   return (
     <>
       <Nav></Nav>
+      <Footer></Footer>
     </>
   );
 }
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/nav";
+import Footer from "../components/footer";
 
 export default function Projects() {
   return (
     <>
       <Nav></Nav>
+      <Footer></Footer>
     </>
   );
 }
